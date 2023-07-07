4d5df4b303b82ef09bcfee9d5503836f30793e67
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-07-08 03:34:46 +0530

    some progress

--- site/app/work/marvel/page.tsx
+++ site/app/work/marvel/page.tsx
@@ -14,7 +14,8 @@ export default function page() {
             "prose dark:prose-invert prose-neutral",
             "prose-headings:font-semibold prose-headings:text-p-0 dark:prose-headings:text-p-9",
             "prose-p:text-p-0 dark:prose-p:text-p-9 prose-p:font-normal prose-p:leading-normal prose-p:text-[15px]",
-            "prose-hr:border-p-6"
+            "prose-hr:border-p-6",
+            "prose-li:text-[15px]"
           )}
         >
           <Mdx />

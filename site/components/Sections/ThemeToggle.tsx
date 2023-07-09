7001d4d3cfbdcd8ae153913efc6ab6b494ee3120
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-07-09 12:52:54 +0530

    theme toggle

--- site/components/Sections/ThemeToggle.tsx
+++ site/components/Sections/ThemeToggle.tsx
@@ -12,7 +12,9 @@ const ThemeToggle = () => {
   return (
     <div className="w-full bg-p-9 dark:bg-p-1 flex justify-center">
       <div className="w-full max-w-4xl p-10 flex justify-between md:justify-start md:gap-5 items-center">
-        <p className="font-medium">{"Prefer Dark Mode? Switch ->"}</p>
+        <p className="font-medium">{`Prefer ${
+          theme?.theme === "dark" ? "Light" : "Dark"
+        } Mode? Switch ->`}</p>
         <IconButton
           variant="text"
           onClick={() =>

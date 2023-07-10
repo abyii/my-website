41381f7e589e7acff292022bfbd8600bec47ba83
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-07-10 22:46:20 +0530

    font size

--- site/components/ui/MdxStyler.tsx
+++ site/components/ui/MdxStyler.tsx
@@ -12,7 +12,7 @@ export const MdxStyler: React.FC<JSX.IntrinsicElements["div"]> = ({
         "w-full max-w-5xl h-full",
         "prose dark:prose-invert prose-neutral",
         "prose-headings:font-semibold prose-headings:text-p-0 dark:prose-headings:text-p-9",
-        "prose-p:text-p-0 dark:prose-p:text-p-9 prose-p:font-normal prose-p:leading-normal prose-p:text-[15px]",
+        "prose-p:text-p-0 dark:prose-p:text-p-9 prose-p:font-normal prose-p:leading-normal prose-p:text-[15.5px] md:prose-p:text-[15px]",
         "prose-hr:border-p-6",
         "prose-li:text-[15px]",
         className

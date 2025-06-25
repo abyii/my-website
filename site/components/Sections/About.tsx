860e802035818e14cdb893b3d67acab65f3e3bbf
abyii <75441358+abyii@users.noreply.github.com>
2025-06-26 00:57:01 +0530

    fix

--- site/components/Sections/About.tsx
+++ site/components/Sections/About.tsx
@@ -7,25 +7,24 @@ const About = () => {
       <div className="max-w-4xl w-full px-10 py-20 font-mono text-sm italic text-p-0 dark:text-p-9">
         <p>
           {/* <span className="not-italic font-semibold">ನಮಸ್ಕಾರ.</span> */}
-          Abhishek, an engineer passionate about building digital experiences
-          from the user-facing code down to the cloud infrastructure that powers
-          them.
+          {
+            "Abhishek, an engineer passionate about building digital experiences from the user-facing code down to the cloud infrastructure that powers them."
+          }
         </p>
         <p className="mt-5">
-          My work spans a wide spectrum of software engineering, where I'm
-          equally intrigued by crafting elegant user interfaces and architecting
-          robust, scalable systems.
+          {
+            "My work spans a wide spectrum of software engineering, where I'm equally intrigued by crafting elegant user interfaces and architecting robust, scalable systems."
+          }
         </p>
         <p className="mt-5">
-          My core toolkit for cloud and DevOps includes: Python, Kubernetes,
-          AWS, OCI, Terraform, and Grafana. On the application side, I'm
-          proficient with TypeScript, React, and Next.js, a combination that
-          gives me a unique, end-to-end perspective on system architecture.
+          {
+            "My core toolkit for cloud and DevOps includes: Python, Kubernetes, AWS, OCI, Terraform, and Grafana. On the application side, I'm proficient with TypeScript, React, and Next.js, a combination that gives me a unique, end-to-end perspective on system architecture."
+          }
         </p>
         <p className="mt-5">
-          I like to guiltlessly explore passions in fields that are often
-          unrelated. Typography, and Sound Design are some of the things that
-          intrigue me.
+          {
+            "I like to guiltlessly explore passions in fields that are often unrelated. Typography, and Sound Design are some of the things that intrigue me."
+          }
         </p>
       </div>
     </div>
